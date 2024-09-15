08d747f9d8599abb13d8ea79d94f30a3b04ee3dd
preatomicprince <tom.cafferata@gmail.com>
2024-09-15 05:37:31 +0100

    added y-sorting player and objects

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     new_room = Room(5, 5)
 
-    new_room.tiles[0].obj = Obj("./res/box1.png")
+    new_room.tiles[12].obj = Obj("./res/box1.png")
 
     while settings.running:
         screen.fill((128, 128, 128))
@@ -25,6 +25,5 @@ if __name__ == "__main__":
         input.update(settings, prev_input)
         player.update(input)
         
-        new_room.draw(screen)
-        player.draw(screen)
+        new_room.draw(screen, player)
         pygame.display.update()
--- src/room.py
+++ src/room.py
@@ -1,3 +1,4 @@
+from player import Player
 from settings import TILE_W, TILE_H, WIDTH, HEIGHT
 from tile import Tile
 
@@ -12,10 +13,18 @@ class Room:
                 #https://clintbellanger.net/articles/isometric_math/ idk
                 self.tiles.append(Tile(((x - y)*(TILE_W/2) + WIDTH/2), ((x + y)*(TILE_H/2) + HEIGHT/2)))
 
-    def draw(self, screen):
+    def draw(self, screen, player):
         for tile in self.tiles:
             tile.draw(screen)
 
         for tile in self.tiles:
             if tile.obj != None:
-                tile.draw_obj(screen)
+                if tile.pos.y + TILE_H/2 < player.pos.y + player.size.y:
+                    tile.draw_obj(screen)
+
+        player.draw(screen) # can be adjusted to work for all characters
+
+        for tile in self.tiles:
+            if tile.obj != None:
+                if tile.pos.y + TILE_H/2 > player.pos.y + player.size.y:
+                    tile.draw_obj(screen)
