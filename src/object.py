799cbcf8e3ccbe0eede614d7557beed9f01cc2a1
preatomicprince <tom.cafferata@gmail.com>
2024-09-16 15:26:20 +0100

    Added object types

--- src/object.py
+++ src/object.py
@@ -1,10 +1,15 @@
 from entity import Ent
+from enum import Enum
 from settings import TILE_H
 
+Obj_Type = Enum(Obj_type, [wall, other]
+
+
 class Obj(Ent):
-    def __init__(self, filepath: str, animation_steps: int = 1, collide: bool = True, interact = False, ind: int = 0):
+    def __init__(self, filepath: str, animation_steps: int = 1, collide: bool = True, interact = False, ind: int = 0, obj_type: Obj_Type = Obj_Type.other):
         self.collide = collide
         self.interact = interact
+        self.obj_type = obj_type
         x_pos = 0
         y_pos = 0
         super().__init__(x_pos, y_pos, filepath, animation_steps, ind)
